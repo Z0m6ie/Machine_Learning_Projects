9b135339aa504f028b2451513f38006adde031d1
Unknown <dwpeabody86@gmail.com>
2017-08-10 20:41:27 -0600

    Main file almost rdy

--- deep-Q-learning/main.py
+++ deep-Q-learning/main.py
@@ -1,20 +1,27 @@
+import sys
 import gym
 from gym import wrappers
+from agent import agent
 
-env = gym.make('Pong-v0')
+batch_size = 32
+episodes = sys.argv[1] if len(sys.argv) > 1 else 5000
+env_name = sys.argv[2] if len(sys.argv) > 2 else "Pong-v0"
+
+env = gym.make(env_name)
 env.reset()
-env = wrappers.Monitor(env, 'Pong-v0', force=True)
-for i_episodes in range():# TODO: )
+env = wrappers.Monitor(env, env_name, force=True)
+for i_episodes in range(episodes):
     state = env.reset()
     while (True):
         env.render()
-        action = agent choose action
+        action = agent.act(state)
         new_state, reward, done, info = env.step(action)
-        store in list(state, action, reward, new_state, done)
+        agent.remember(state, action, reward, new_state, done)
         state = new_state
-        if done:
-            print("Game finished after {} episodes".format(i_episode+1))
-            break
-    #Carry out mini-batch update on neural network
+    agent.memory_replay(batch_size)
+    if done:
+        print("Game finished after {} episodes".format(i_episodes + 1))
+        agent.save_model()
+        break
 
 env.close()
