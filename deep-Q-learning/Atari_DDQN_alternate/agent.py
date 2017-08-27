731518998ed1297bf2750065cb73724313cd4302
Unknown <dwpeabody86@gmail.com>
2017-08-26 23:15:10 -0600

    test stuff

--- deep-Q-learning/Atari_DDQN_alternate/agent.py
+++ deep-Q-learning/Atari_DDQN_alternate/agent.py
@@ -5,19 +5,19 @@ from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
 from keras.optimizers import Adam
-sizes = (80, 80, 4)
+sizes = (84, 84, 4)
 
 
 class Agent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=400000)
+        self.memory = deque(maxlen=1000000)
         self.gamma = 0.99   # discount rate
         self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01  # exploration will not decay futher
-        self.epsilon_decay = 0.00033
-        self.learning_rate = 0.001
+        self.epsilon_min = 0.025  # exploration will not decay futher
+        self.epsilon_decay = 0.00024375
+        self.learning_rate = 0.00025
         self.loss = 0
         self.model = self._build_model()
         self.target_model = self._build_model()
@@ -57,7 +57,7 @@ class Agent:
     def RGBprocess(self, raw_img):
         processed_observation = Image.fromarray(raw_img, 'RGB')
         processed_observation = processed_observation.convert('L')
-        processed_observation = processed_observation.resize((80, 80))
+        processed_observation = processed_observation.resize((84, 84))
         processed_observation = np.array(processed_observation)
         processed_observation = processed_observation.reshape(
             1, processed_observation.shape[0], processed_observation.shape[1])
@@ -65,11 +65,11 @@ class Agent:
 
     def stack(self, processed_observation):
         I_4 = self.old_I_3 if self.old_I_3 is not None else np.zeros(
-            (1, 80, 80))
+            (1, 84, 84))
         I_3 = self.old_I_2 if self.old_I_2 is not None else np.zeros(
-            (1, 80, 80))
+            (1, 84, 84))
         I_2 = self.old_I_1 if self.old_I_1 is not None else np.zeros(
-            (1, 80, 80))
+            (1, 84, 84))
         I_1 = processed_observation
         processed_stack = np.stack((I_4, I_3, I_2, I_1), axis=3)
         self.old_I_4 = I_4
@@ -79,7 +79,7 @@ class Agent:
         return processed_stack
 
     def remember(self, state, action, reward, new_state, done):
-        if len(self.memory) >= 400000:
+        if len(self.memory) >= 1000000:
             self.memory.popleft()
             self.memory.append([state, action, reward, new_state, done])
         else:
@@ -94,7 +94,8 @@ class Agent:
             if not done:
                 future_action = self.model.predict(new_state)[0]
                 future_q = self.target_model.predict(new_state)[0]
-                target = reward + self.gamma * future_q[np.argmax(future_action)]
+                target = reward + self.gamma * \
+                    future_q[np.argmax(future_action)]
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
--- deep-Q-learning/Atari_DDQN_alternate/main.py
+++ deep-Q-learning/Atari_DDQN_alternate/main.py
@@ -2,6 +2,7 @@ import sys
 import gym
 from gym import wrappers
 from agent import Agent
+import numpy as np
 
 
 batch_size = 32
@@ -10,8 +11,8 @@ env_name = sys.argv[2] if len(sys.argv) > 2 else "Breakout-v0"
 
 episodes = int(episodes)
 env_name = env_name
-D = 80 * 80
-
+D = 84 * 84
+score = []
 text_file = open("Output.txt", "w")
 
 env = gym.make(env_name)
@@ -27,8 +28,8 @@ for i_episodes in range(episodes):
     totalreward = 0
     done = False
     while not done:
-        if i_episodes % 50 == 0:
-            env.render()
+        #if i_episodes % 50 == 0:
+            #env.render()
         action = agent.act(state)
         new_state, reward, done, info = env.step(action)
         new_state = agent.RGBprocess(new_state)
@@ -41,7 +42,11 @@ for i_episodes in range(episodes):
         text_file.write("{} episode, score = {}\n".format(
             i_episodes + 1, totalreward))
         agent.save_model()
-        if i_episodes % 1 == 0:
+        score.append(totalreward)
+        if i_episodes % 100 == 0:
+            print("{} episode, score = {}, rolling mean 100 episodes = {}".format(
+                i_episodes + 1, totalreward, np.mean(score[-100:])))
+        if i_episodes % 40 == 0:
             agent.update_target_model()
 
 env.close()
